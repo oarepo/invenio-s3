112fa9ab018ad7127a7a24f0d4dec8307cd59b5d
Mirek Simek <miroslav.simek@gmail.com>
2025-04-08 07:58:03 +0200

    Multipart upload support
    * Implementation of multipart upload, as described in RFC 0072
    
    * See inveniosoftware/rfcs#91
    
    Co-authored-by: Mirek Simek <miroslav.simek@cesnet.cz>

--- invenio_s3/config.py
+++ invenio_s3/config.py
@@ -87,3 +87,12 @@ In some cases, specially those not using AWS S3, some extra configuration might 
     }
 
 """
+
+S3_UPLOAD_URL_EXPIRATION = 3600 * 24 * 7
+"""Number of seconds the file upload URL will be valid. The default here is 7 days
+to allow large file uploads with large number of chunks to be completed. This is
+currently the maximum allowed by the AWS.
+See `Amazon Boto3 documentation on presigned URLs
+<https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.generate_presigned_url>`_
+for more information.
+"""
